67973cec8166b0fa2643b44b9105af5b8e701bf3
MELODY SIMWA <simwamelody@gmail.com>
2024-11-25 12:28:52 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+
+const element = document.createElement("div");
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+// Optionally set its text content
+newHeader.textContent = "Melody is the champion ";
+
+// Append it to the DOM
+document.body.appendChild(newHeader);
+
+document.body.append(element);
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
+  element.append(ul);
